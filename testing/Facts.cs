9a11d4cb611401d9242126c0aaf2534bba27aaee
Dusan Jovanovic <Dusan.Jovanovic@Clarivate.com>
2023-02-10 11:15:52 +0100

    after big reshuffle all seems to work

--- testing/Facts.cs
+++ testing/Facts.cs
@@ -1,13 +1,27 @@
 ﻿// make sure this is not built with dotnet benchmark projects
-
+#define USE_RANDOM_STRING_SPECIMEN
 using System.Diagnostics;
 using System.Text;
 using Xunit;
+using Xunit.Abstractions;
+using System.Runtime.CompilerServices;
 
 namespace gzipstream;
 public class Tests_are_here
 {
+    private readonly ITestOutputHelper output;
     static readonly string originalString = string.Empty;
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    void writeln(string txt)
+    {
+        output.WriteLine(txt);
+    }
+
+    public Tests_are_here(ITestOutputHelper output)
+    {
+        this.output = output;
+    }
     static Tests_are_here()
     {
 #if USE_RANDOM_STRING_SPECIMEN
@@ -20,38 +34,58 @@ public class Tests_are_here
 #endif
     }
 
+#if USE_RANDOM_STRING_SPECIMEN
     [Fact]
-    public void Test_Gzip()
+    public void Test_64bit_encoding()
     {
-        DBJcore.Writeln("GZip compression testing ---------------------------------------------------");
+        writeln("Test_64bit_encoding ---------------------------------------------------");
 
-        DBJcore.Writeln("Length of original string: " + originalString.Length);
+        writeln("Length of original UrlEncodedRandomString: " + originalString.Length);
         byte[] dataToCompress = Encoding.UTF8.GetBytes(originalString);
         byte[] compressedData = GZipCompressor.Compress(dataToCompress);
 
         string compressedString = Encoding.UTF8.GetString(compressedData);
-        DBJcore.Writeln("Length of compressed string: " + compressedString.Length);
+        writeln("Length of GZIP compressed UrlEncodedRandomString: " + compressedString.Length);
         byte[] decompressedData = GZipCompressor.Decompress(compressedData);
         string deCompressedString = Encoding.UTF8.GetString(decompressedData);
-        DBJcore.Writeln("Length of decompressed string: " + deCompressedString.Length);
+        writeln("Length of decompressed UrlEncodedRandomString: " + deCompressedString.Length);
+
+        Assert.Equal(originalString.Length, deCompressedString.Length);
+
+    }
+#endif
+    [Fact]
+    public void Test_Gzip()
+    {
+        writeln("GZip compression testing ---------------------------------------------------");
+
+        writeln("Length of original string: " + originalString.Length);
+        byte[] dataToCompress = Encoding.UTF8.GetBytes(originalString);
+        byte[] compressedData = GZipCompressor.Compress(dataToCompress);
+
+        string compressedString = Encoding.UTF8.GetString(compressedData);
+        writeln("Length of compressed string: " + compressedString.Length);
+        byte[] decompressedData = GZipCompressor.Decompress(compressedData);
+        string deCompressedString = Encoding.UTF8.GetString(decompressedData);
+        writeln("Length of decompressed string: " + deCompressedString.Length);
 
         Assert.Equal(originalString.Length, deCompressedString.Length);
 
     }
     [Fact]
-    static public void Test_Deflator()
+    public void Test_Deflator()
     {
-        DBJcore.Writeln("DeflateStream compression testing ---------------------------------------------------");
+        writeln("DeflateStream compression testing ---------------------------------------------------");
 
-        DBJcore.Writeln("Length of original string: " + originalString.Length);
+        writeln("Length of original string: " + originalString.Length);
         byte[] dataToCompress = Encoding.UTF8.GetBytes(originalString);
         byte[] compressedData = DeflatorStringCompression.Compress(dataToCompress);
 
         string compressedString = Encoding.UTF8.GetString(compressedData);
-        DBJcore.Writeln("Length of compressed string: " + compressedString.Length);
+        writeln("Length of compressed string: " + compressedString.Length);
         byte[] decompressedData = DeflatorStringCompression.Decompress(compressedData);
         string deCompressedString = Encoding.UTF8.GetString(decompressedData);
-        DBJcore.Writeln("Length of decompressed string: " + deCompressedString.Length);
+        writeln("Length of decompressed string: " + deCompressedString.Length);
 
         Assert.Equal(originalString.Length, deCompressedString.Length);
 
@@ -61,21 +95,21 @@ public class Tests_are_here
     {
         // this goes to file thus it is perfectly usable from 
         // unit tests, unlike Console which is not
-        DBJcore.Writeln("Brotli compression testing ------------------------------------------");
-        DBJcore.Writeln("Length of original string: " + originalString.Length);
+        writeln("Brotli compression testing ------------------------------------------");
+        writeln("Length of original string: " + originalString.Length);
         /// transform to UTF8 byte []
         byte[] dataToCompress = System.Text.Encoding.UTF8.GetBytes(originalString);
         /// compress the byte []
         byte[] compressedData = Brotli.Compress(dataToCompress) ;
         /// arrive to string 
         string compressedString = Convert.ToBase64String(compressedData);
-        DBJcore.Writeln("Length of compressed string: " + compressedString.Length);
+        writeln("Length of compressed string: " + compressedString.Length);
 
         /// decompress the byte []
         byte[] decompressedData = Brotli.Decompress(compressedData);
         /// gt the string and its length
         string deCompressedString = Convert.ToBase64String(decompressedData);
-        DBJcore.Writeln("Length of decompressed string: " + deCompressedString.Length);
+        writeln("Length of decompressed string: " + deCompressedString.Length);
 
         // this is net core
         //Debug.Assert(originalString.Length == deCompressedString.Length);
